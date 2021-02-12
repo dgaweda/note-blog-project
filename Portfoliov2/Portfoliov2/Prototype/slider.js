a6558163240da6798f63e38e565a7a2ff3ce4015
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-02-12 21:03:13 +0100

    New page - Notes Added: - new page Notes - some css to it - new references to aboutMe - new title of app + ico + Done some refactor

--- Portfoliov2/Portfoliov2/Prototype/slider.js
+++ Portfoliov2/Portfoliov2/Prototype/slider.js
@@ -10,5 +10,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
