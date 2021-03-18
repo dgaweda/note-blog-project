947c8c5928807f5ea965d5288f3c33e6b266901d
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-03-18 13:20:28 +0100

    Some changes In AboutMe - New features in aboutme controller - Rempved HomeViewmodel From AboutMe Controller - CHnges some data in DB __NotWorkingNeedToRepairTheRoutes
    _userInformations dint get a sungal from controller thats the reason why app doeasnt launch at aboutME Controller

--- Portfolio/ViewModels/AboutMeViewModel.cs
+++ Portfolio/ViewModels/AboutMeViewModel.cs
@@ -8,11 +8,9 @@ namespace Portfolio.ViewModels
 {
     public class AboutMeViewModel
     {
-        public IEnumerable<Person> People { get; set; }
+        public Person People { get; set; }
         public IEnumerable<Hobby> Hobbies { get; set; }
-        public IEnumerable<HobbyToPerson> PersonHobbies { get; set; }
         public IEnumerable<Education> Education { get; set; }
-        public IEnumerable<EducationToPerson> PersonSchools { get; set; }
         public IEnumerable<Technology> PersonTechnologies { get; set; }
     }
 }
